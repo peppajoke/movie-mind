cf2a2b81315b1553a17a263a476bdb18fc0d624f
peppajoke <peppajoke@users.noreply.github.com>
2024-03-11 18:00:42 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for movie_mind package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("movie_mind", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="movie_mind",
+    version=read("movie_mind", "VERSION"),
+    description="Awesome movie_mind created by peppajoke",
+    url="https://github.com/peppajoke/movie-mind/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="peppajoke",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["movie_mind = movie_mind.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
